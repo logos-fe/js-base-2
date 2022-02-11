2d3b9697994c17181d78c9bff69fbf0c55dc83e9
khhhrystyyna <khrystyna.styslo@gmail.com>
2022-02-11 22:14:34 +0200

    js base 2

---
+++ script.js
@@ -0,0 +1,75 @@
+// const world = " world"
+
+const password = "123456"
+
+// alert("Hello" + world)
+// const age = prompt("How old are you?", "20")
+// alert("Your age is " + age)
+
+// const years = confirm("Are you 18 years old?")
+
+// console.log("Years: ", years, world)
+
+// const number1 = "6"
+// const number2 = "2"
+
+// const result = +number1 + +number2
+// The same result
+// const result = Number(number1) + Number(number2)
+
+// alert(result)
+
+// Інкремент - ++
+// Декремент - --
+
+// let number = 1
+// number = number + 1
+// The same
+// number += 5 // number is 6
+// number -= 2 // number is 4
+
+// number++
+// number--
+
+// example = 1
+// const example = number--
+// example = 0
+// const example = --number
+
+// console.log('example', example, number)
+
+// const number1 = 5
+// const number2 = 5
+
+// const word1 = "Ka"
+// const word2 = "Ku"
+
+// console.log('result: ', word1 > word2)
+
+const age = prompt("How old are you?", "18")
+
+// const result = age >= 12 && age < 18
+// const result2 = Number(age) === 12 || +age === 18 || +age === 15
+
+// console.log('result2', result2)
+
+// if (age > 0 && age < 12) {
+//     alert('You are child')
+// } else if (age >= 12 && age < 18) {
+//     alert('You are teenager')
+// } else if (age >= 18 && age < 65) {
+//     alert('You are adult')
+// } else {
+//     alert('You are pensioner')
+// }
+
+
+
+
+
+
+
+
+
+
+
